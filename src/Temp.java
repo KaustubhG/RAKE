71f6a3284f63630423eafae701ddf65f136cfb1f
Vandan-V-Phadke <one.donerocks@gmail.com>
2015-04-05 22:03:34 +0530

    Added 10 tests

--- src/Temp.java
+++ src/Temp.java
@@ -14,35 +14,21 @@ public class Temp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		String test = " , hey, this is, kg";
-		String delimiter = "\\s*,\\s*|\\s+";
-		for(String a : test.split(delimiter)){
-			System.out.println(a);
-		}
+		String test = "12 vandan 14 kg 21 hello (abcd) (efgh)";
 		
-		/*
-		StringBuilder build = new StringBuilder();
-		String sCurrentLine;
-		try (BufferedReader br = new BufferedReader(new FileReader("string.txt")))
-		{		
-			while ((sCurrentLine = br.readLine()) != null) {
-				build.append(sCurrentLine).append(" ");
-			}
-
-		} catch (IOException e) {
-			e.printStackTrace();
-		} 
-
-		GenerateCandidateKeywords genKey = new GenerateCandidateKeywords(build.toString());
+		GenerateCandidateKeywords genKey = new GenerateCandidateKeywords(test.toString());
 		ArrayList<ArrayList<String>> candKeyWords = genKey.generate();
-		for(ArrayList<String> list : candKeyWords){
-			System.out.println(list);
-		}
+
 
 		GenerateWordScores wordScores = new GenerateWordScores(candKeyWords);
 		System.out.println(wordScores.getWordScore());
-		*/
 
-
+/*		//print top n keyWords
+		int n = 7;
+		List<Entry<ArrayList<String>, Double>> greatest = Main.findGreatest(wordScores.getWordScore(), n);
+		//System.out.println("Top "+n+" entries:");
+		for (Entry<ArrayList<String>, Double> entry : greatest){
+			System.out.println(entry);
+		}*/
 	}
 }
